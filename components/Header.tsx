5730882c47b8a5f231dc31d31fce5133ac230b88
pallavverma <pallavpv14730@gmail.com>
2025-12-06 22:43:21 +0530

    Header Section Build Successfully

---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Activities } from "@/lib/Data";
+import { ArrowBigDown, ArrowDown01Icon, ChevronDown } from "lucide-react";
+import { useState } from "react";
+
+function Header() {
+  const [activeTab, setActiveTab] = useState(1);
+  return (
+    <div className="p-2 bg-[#f3f4f6] space-y-4  shadow-md">
+      <div className="grid grid-cols-7 rounded-xl bg-[#E7ECF6] px-2 py-1">
+        {Activities.map((activity) => {
+          const isActive = activeTab === activity.id;
+          return (
+            <div key={activity.id} onClick={() => setActiveTab(activity.id)}>
+              <div
+                className={`text-center text-xs py-1 font-medium cursor-pointer rounded-md
+                transition-all duration-200 ${
+                  isActive
+                    ? "bg-white text-[#2E3A59] shadow-md border-b-2 border-[#4A90E2]"
+                    : "text-[#2E3A59] hover:bg-[#F5F7FB]"
+                } `}
+              >
+                {activity.title}
+              </div>
+            </div>
+            
+          );
+          
+        })}
+      </div>
+      <div className="grid grid-cols-10 items-center justify-center gap-2 ">
+        {Activities.find((act) => act.id === activeTab)?.Subactivities?.map(
+          (Subactivity) => {
+            return <div key={`${activeTab}-${Subactivity.id}`} style={{backgroundColor:Subactivity.color}} className={`text-xs text-white py-1  rounded-md   text-center cursor-pointer`}>{Subactivity.title}</div>;
+          }
+        )}
+      </div>
+    </div>
+  );
+}
+export default Header;
---
+++ lib/Data.ts
@@ -0,0 +1,175 @@
+export const Activities = [
+  {
+    id: 1,
+    title: "IM Activites",
+    Subactivities: [
+      {
+        id: 1,
+        title: "IM Activity",
+        color: "#15803d",
+      },
+      {
+        id: 2,
+        title: "PI Activity",
+        color: "#eab308",
+      },
+      {
+        id: 3,
+        title: "Knowledge Transition",
+        color: "#e879f9",
+      },
+      {
+        id: 4,
+        title: "Documentation",
+        color: "#a78bfa",
+      },
+      {
+        id: 5,
+        title: "Customer Meeting",
+        color: "#c084fc",
+      },
+      {
+        id: 6,
+        title: "Internal Meeting",
+        color: "#15803d",
+      },
+      {
+        id: 7,
+        title: "Sev9",
+        color: "#fb923c",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Non IM Activites",
+    Subactivities: [
+      {
+        id: 1,
+        title: "Knowledge Transition",
+        color: "#e879f9",
+      },
+      {
+        id: 2,
+        title: "Core Activities",
+        color: "#1e293b",
+      },
+      {
+        id: 3,
+        title: "Call List",
+        color: "#86198f",
+      },
+      {
+        id: 4,
+        title: "Application Maintaince",
+        color: "#155e75",
+      },
+      {
+        id: 5,
+        title: "PI Activity",
+        color: "#eab308",
+      },
+      {
+        id: 6,
+        title: "Non IM Activity",
+        color: "#18181b",
+      },
+      {
+        id: 7,
+        title: "Documentation",
+        color: "#a78bfa",
+      },
+      {
+        id: 8,
+        title: "Customer Meeting",
+        color: "#c084fc",
+      },
+      {
+        id: 9,
+        title: "Internal/Team Meeting",
+        color: "#60a5fa",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Enhancement",
+    Subactivities: [
+      {
+        id: 1,
+        title: "Requirement",
+        color: "#15803d",
+      },
+      {
+        id: 2,
+        title: "Analysis",
+        color: "#fb923c",
+      },
+      {
+        id: 3,
+        title: "Enhancement < 80 hrs",
+        color: "#1e293b",
+      },
+      {
+        title: "Enhancement > 80 hrs",
+        color: "#86198f",
+      },
+      {
+        id: 5,
+        title: "Customer Meeting",
+        color: "#60a5fa",
+      },
+      {
+        id: 6,
+        title: "Coding",
+        color: "#ef4444",
+      },
+      {
+        id: 7,
+        title: "Internal/Team Meeting",
+        color: "#ef4444",
+      },
+      {
+        id: 8,
+        title: "Testing",
+        color: "#ef4444",
+      },
+      {
+        id: 9,
+        title: "Implementation",
+        color: "#ef4444",
+      },
+      {
+        id: 10,
+        title: "Post Implementation",
+        color: "#ef4444",
+      },
+      {
+        id: 11,
+        title: "Documentation",
+        color: "#ef4444",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Others",
+    Subactivities: [
+      {
+        id: 1,
+        title: "Leave",
+        color: "#15803d",
+      },
+      {
+        id: 2,
+        title: "Break",
+        color: "#fb923c",
+      },
+      {
+        id: 3,
+        title: "Self Learning",
+        color: "#60a5fa",
+      },
+    ],
+  },
+];
