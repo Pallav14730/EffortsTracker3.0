676f68f5f0827862e4dab6714f56f9a64d294d90
pallavverma <pallavpv14730@gmail.com>
2025-12-06 22:48:14 +0530

    Footer Section Build Successfully

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return (
+        <footer className="flex justify-center items-center space-x-4 p-1 bg-linear-to-b from-[#4e0ecf] to-[#2e005d] text-white font-bold">
+        <h1>
+          Copyright @ 2025-2026 <span>TCS</span>.All rights reserved
+        </h1>
+      </footer>
+    )
+}
+export default Footer;
