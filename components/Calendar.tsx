800d5526a551a87c0d3b60240485a85a918e6f20
pallavverma <pallavpv14730@gmail.com>
2025-12-07 20:40:40 +0530

    Added the Drag and Drop Feature

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -4,16 +4,41 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import { Draggable } from "@fullcalendar/interaction";
+import { useEffect } from "react";
 
 export default function CalendarPage() {
+  useEffect(() => {
+    let container = document.getElementById("external-events");
+
+    if (container) {
+      new Draggable(container, {
+        itemSelector: ".fc-draggable",
+        eventData: function (eventEl) {
+          const data = JSON.parse(eventEl.getAttribute("data-json")!);
+
+          return {
+            title: data.subactivity,
+            color: data.color,
+            custom: data,
+          };
+
+        },
+
+      });
+    }
+  }, []);
+
   return (
     <div className="pt-[120px] h-screen w-full">
-      <div className=" bg-white rounded-2xl shadow-xl mx-3 h-full ">
+      <div className=" bg-white rounded-2xl shadow-xl  mx-3 h-full ">
         <FullCalendar
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           initialView="dayGridMonth"
           selectable={true}
           nowIndicator={true}
+          droppable={true}
+          editable={true}
           displayEventTime={false}
           dragScroll={true}
           views={{
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,20 @@
 import { Activities } from "@/lib/Data";
 import { ArrowBigDown, ArrowDown01Icon, ChevronDown } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
+import { act, useState } from "react";
 
 function Header() {
   const [activeTab, setActiveTab] = useState(1);
+  function dragstart(e: any) {
+    setTimeout(() => {
+      document.getElementById("header-wrapper")!.style.pointerEvents = "none";
+    }, 0);
+  }
+  function dragend() {
+    document.getElementById("header-wrapper")!.style.pointerEvents = "auto";
+  }
   return (
-    <div className="p-2 fixed top-0 left-0 z-40 w-full bg-[#f3f4f6] space-y-4   shadow-md">
+    <div id="header-wrapper" className="p-2 bg-[#f3f4f6] space-y-4 shadow-md fixed top-0 left-0 w-full z-40">
       <div className="grid grid-cols-7 rounded-xl bg-[#E7ECF6] px-2 py-1">
         {Activities.map((activity) => {
           const isActive = activeTab === activity.id;
@@ -15,11 +23,10 @@ function Header() {
             <div key={activity.id} onClick={() => setActiveTab(activity.id)}>
               <div
                 className={`text-center text-xs py-1 font-medium cursor-pointer rounded-md
-                transition-all duration-200 ${
-                  isActive
+                transition-all duration-200 ${isActive
                     ? "bg-white text-[#2E3A59] shadow-md border-b-2 border-[#4A90E2]"
                     : "text-[#2E3A59] hover:bg-[#F5F7FB]"
-                } `}
+                  } `}
               >
                 {activity.title}
               </div>
@@ -32,14 +39,27 @@ function Header() {
           </Link>
         </div>
       </div>
-      <div className="grid grid-cols-10 items-center justify-center gap-2 ">
+      <div id="external-events" className="flex flex-wrap items-center justify-center gap-2 pb-2">
         {Activities.find((act) => act.id === activeTab)?.Subactivities?.map(
           (Subactivity) => {
             return (
               <div
+
+                id={Subactivity.id?.toString()}
+                title={Subactivity.title}
+                data-activity={Activities.find((act) => act.id === activeTab)?.title}
+                draggable="true"
+                data-json={JSON.stringify({
+                  id: Subactivity.id,
+                  subactivity: Subactivity.title,
+                  activity: Activities.find((act) => act.id === activeTab)?.title,
+                  color: Subactivity.color,
+                })}
+                onDragStart={dragstart}
+                onDragEnd={dragend}
                 key={`${activeTab}-${Subactivity.id}`}
                 style={{ backgroundColor: Subactivity.color }}
-                className={`text-xs text-white py-1  rounded-md   text-center cursor-pointer`}
+                className="fc-draggable text-xs text-white py-1 px-3 rounded-md text-center cursor-pointer"
               >
                 {Subactivity.title}
               </div>
