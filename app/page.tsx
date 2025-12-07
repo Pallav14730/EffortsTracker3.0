9f62d887bbd4269efcb8bda2f614cfb57384144a
pallavverma <pallavpv14730@gmail.com>
2025-12-07 12:12:15 +0530

    Successfully Merged Footer with Main

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-import CalendarPage from "@/components/Calendar";
-
-export default function Home() {
-  return (
-    <div>
-      <CalendarPage />
-=======
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
       <Header />
       <Footer />
->>>>>>> footer
     </div>
   );
 }
